dcb82b38a350a58d92992068c42b8bf7af2a7ae8
sa212320 <sa212320@gmail.com>
2017-02-07 02:22:14 +0800

    ng-model
    ng-model

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Parties } from '../../../../collections/parties';
+import { name as PartyAdd } from '../partyAdd/partyAdd';
 import template from './partiesList.html';
 
 class PartiesList {
@@ -21,7 +22,8 @@ const name = 'partiesList';
  
 // create a module
 export default angular.module(name, [
-  angularMeteor
+  angularMeteor,
+  PartyAdd
 ]).component(name, {
   template,
   controllerAs: name,
---
+++ imports/ui/components/partyAdd/partyAdd.js
@@ -0,0 +1,25 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+ 
+import template from './partyAdd.html';
+ 
+class PartyAdd {
+  constructor() {
+    this.party = {};
+  }
+ 
+  submit() {
+    console.log('submit:', this.party);
+  }
+}
+ 
+const name = 'partyAdd';
+ 
+// create a module
+export default angular.module(name, [
+  angularMeteor
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: PartyAdd
+});
