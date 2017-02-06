a8f292383581fbf23097389adf30a7557ec8cc54
sa212320 <sa212320@gmail.com>
2017-02-07 00:19:01 +0800

    init

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Parties } from '../collections/parties';
+ 
+angular.module('socially', [
+    angularMeteor
+  ])
+  .controller('PartiesListCtrl', function($scope) {
+    'ngInject';
+ 
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
+  });
