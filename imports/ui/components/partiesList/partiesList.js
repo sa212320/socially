c518aedc5b20b290abd351c692330bed83b179c0
sa212320 <sa212320@gmail.com>
2017-02-07 13:18:55 +0800

    Routing
    Routing

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 import template from './partiesList.html';
 import { Parties } from '../../../api/parties';
 import { name as PartyAdd } from '../partyAdd/partyAdd';
@@ -25,9 +26,20 @@ const name = 'partiesList';
 export default angular.module(name, [
   angularMeteor,
   PartyAdd,
+  uiRouter,
   PartyRemove
 ]).component(name, {
   template,
   controllerAs: name,
   controller: PartiesList
-});
+})
+  .config(config);
+ 
+function config($stateProvider) {
+  'ngInject';
+  $stateProvider
+    .state('parties', {
+      url: '/parties',
+      template: '<parties-list></parties-list>'
+    });
+}
